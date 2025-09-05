d62719858bdf159bb5d1c798ef1095e9c214fdab
DESKTOP-LGF9G09\rav <ravusz91@gmail.com>
2025-09-05 22:32:54 +0200

    add ng20

---
+++ angular-elements-example/src/app/test-component/test-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-component',
+  templateUrl: './test-component.html',
+  styleUrls: ['./test-component.scss'],
+  standalone: true,
+})
+export class TestComponent { }
--- host-ng8/src/app/app.module.ts
+++ host-ng8/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -22,5 +22,6 @@ import { ConfirmationService } from "primeng/api";
   ],
   providers: [MessageService, ConfirmationService],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule {}
+export class AppModule { }
